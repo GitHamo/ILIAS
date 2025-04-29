bc8d72aa6020b6019d28cf543a5f10ba87e5084d
sergio santiago <sergiosantiago0403@gmail.com>
2025-04-29 15:05:15 +0200

    [LTI] Fix: save not attended status
    These changes address an issue that prevented learning progress status from being saved correctly even when the test is not attended.
    
    Associated mantis ticket:
    https://mantis.ilias.de/view.php?id=44985

--- components/ILIAS/Tracking/classes/status/class.ilLPStatusLtiOutcome.php
+++ components/ILIAS/Tracking/classes/status/class.ilLPStatusLtiOutcome.php
@@ -48,7 +48,9 @@ class ilLPStatusLtiOutcome extends ilLPStatus
             $object = $this->ensureObject($a_obj_id, $a_obj);
             $ltiMasteryScore = $object->getMasteryScore();
 
-            if ($ltiResult->getResult() >= $ltiMasteryScore) {
+            if ($ltiResult->getResult() == null) {
+                return self::LP_STATUS_NOT_ATTEMPTED_NUM;
+            } elseif ($ltiResult->getResult() >= $ltiMasteryScore) {
                 return self::LP_STATUS_COMPLETED_NUM;
             } else {
                 return self::LP_STATUS_FAILED_NUM;
