1e972c104639864bed63ba7caff12086c2deb891
Fabian Schmid <fabian@sr.solutions>
2025-06-10 08:04:06 +0200

    Revert "[FIX] 0043730: Failed test: MainControls - System Info: show"
    This reverts commit 87ddc3d93cacfd6d57e9dd78366497930d1c4969.

--- components/ILIAS/UI/resources/js/MainControls/system_info.js
+++ components/ILIAS/UI/resources/js/MainControls/system_info.js
@@ -4,59 +4,60 @@ il.UI.maincontrols = il.UI.maincontrols || {};
 
 (function ($, maincontrols) {
   maincontrols.system_info = (function ($) {
-    let calculating = false;
-
-    const maybeShowMoreButton = function (item, moreButton) {
-      calculating = true;
-      const content = item.find('.il-system-info-content');
-      const itemHeight = item.prop('offsetHeight');
-      const contentHeight = content.prop('offsetHeight');
-
-      if (contentHeight > itemHeight) {
-        moreButton.show();
-      } else {
-        moreButton.hide();
-      }
-      calculating = false;
-    };
-
+    var calculating = false;
     /**
      * decide and init condensed/wide version
      */
-    const init = function (id) {
-      const item = $(`#${id}`);
-      const moreButton = item.find('.il-system-info-more');
-      moreButton.click(() => {
-        item.toggleClass('full');
-        moreButton.hide();
-      });
-
-      maybeShowMoreButton(item, moreButton);
-      $(window).resize(() => {
+    var init = function (id) {
+      listener(id);
+      $(window).resize(function () {
         if (!calculating) {
-          maybeShowMoreButton(item);
+          listener(id);
         }
       });
     };
 
-    const close = function (id) {
-      const element = $(`#${id}`);
-      const close_uri = decodeURI(element.data('closeUri'));
+    var listener = function (id) {
+      calculating = true;
+      let item = $('#' + id);
+      let content = item.find('.il-system-info-content');
+      let item_height = item.prop('offsetHeight');
+      let content_height = content.prop('offsetHeight');
+      let more_button = item.find('.il-system-info-more');
+
+      if (content_height > item_height) {
+        more_button.show();
+        more_button.click(function () {
+          item.toggleClass('full');
+          more_button.hide();
+        });
+      } else {
+        more_button.hide();
+        more_button.unbind();
+      }
+      calculating = false;
+    };
+
+    var close = function (id) {
+      let element = $('#' + id);
+      let close_uri = decodeURI(element.data('closeUri'));
       $.ajax({
         async: false,
         type: 'GET',
         url: close_uri,
-        success(data) {
+        success: function (data) {
           element.slideUp(500, function () {
             $(this).remove();
           });
-        },
+        }
       });
     };
 
     return {
-      init,
-      close,
-    };
-  }($));
-}($, il.UI.maincontrols));
+      init: init,
+      close: close,
+    }
+
+  })($);
+})($, il.UI.maincontrols);
+
