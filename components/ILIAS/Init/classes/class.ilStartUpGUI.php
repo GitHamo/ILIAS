55a66d3359af9d28afed6cfacc2944fd5bb474c1
Sergio Santiago Sánchez <54277294+sergiosant02@users.noreply.github.com>
2025-10-06 11:25:08 +0200

    [LTI] Fix: add doLTIAuthentication to safe commands (#10198)
    In version 10.1 of ILIAS, it was possible to execute a command in a POST request even if the request did not include a CSRF token. Change introduced in: https://github.com/surlabs/ILIAS/commit/a0696288b58df8d248261a0104f68960b5856b5d
    
    Following the recent changes introduced in version 10.2, aimed at improving the robustness of request handling, any POST request must now include a CSRF token, or the command to be executed must be part of the list of secure commands. Due to the nature of LTI, it is necessary to include the LTI authentication command in the list of secure commands.

--- components/ILIAS/Init/classes/class.ilStartUpGUI.php
+++ components/ILIAS/Init/classes/class.ilStartUpGUI.php
@@ -150,6 +150,7 @@ class ilStartUpGUI implements ilCtrlBaseClassInterface, ilCtrlSecurityInterface
     {
         return [
             'doStandardAuthentication',
+            'doLTIAuthentication'
         ];
     }
 
