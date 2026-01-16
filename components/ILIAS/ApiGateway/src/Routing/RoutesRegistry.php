9900be4f29eccfc99d2aed623eb51f33b7f3c70c
Ahmed Hamouda <ahmed.hamouda@minervis.com>
2026-01-16 22:01:20 +0100

    add RoutesRegistryFactory and refactor RoutesRegistry

--- components/ILIAS/ApiGateway/src/Routing/RoutesRegistry.php
+++ components/ILIAS/ApiGateway/src/Routing/RoutesRegistry.php
@@ -26,37 +26,26 @@ use LogicException;
 
 class RoutesRegistry
 {
-    private static ?self $instance = null;
+    private array $routes;
 
-    /** @var array<string, \ILIAS\ApiGateway\Routing\Route> */
-    private array $routes = [];
-
-    private function __construct()
+    public function __construct(array $routes)
     {
+        array_walk($routes, [$this, 'register']);
     }
 
     /**
-     * @codeCoverageIgnore because any test would fail before this line is executed
+     * @return array<string, \ILIAS\ApiGateway\Routing\Route>
      */
-    private function __clone()
+    public function all(): array
     {
-    }
-
-    public function __wakeup(): void
-    {
-        throw new LogicException("Cannot unserialize a singleton.");
-    }
-
-    public static function getInstance(): self
-    {
-        return self::$instance ??= new self();
+        return $this->routes;
     }
 
     /**
      * Registers a route for all of its specified HTTP methods.
      * Throws an exception if any method/path combination is already registered.
      */
-    public function register(Route $route): void
+    private function register(Route $route): void
     {
         $path = $route->getPath();
         $method = $route->getMethod();
@@ -70,30 +59,8 @@ class RoutesRegistry
         if (empty($method)) {
             throw new InvalidArgumentException("Cannot register a route with no HTTP methods for path '{$path}'.");
         }
-        
+
         $this->routes[$key] = $route;
-    }
-
-    public function get(string $method, string $path): ?Route
-    {
-        $key = $this->getInternalKey($method, $path);
-
-        return $this->routes[$key] ?? null;
-    }
-
-    /**
-     * @return array<string, \ILIAS\ApiGateway\Routing\Route>
-     */
-    public function all(): array
-    {
-        return $this->routes;
-    }
-
-    public function has(string $method, string $path): bool
-    {
-        $key = $this->getInternalKey($method, $path);
-
-        return isset($this->routes[$key]);
     }
 
     private function getInternalKey(string $method, string $path): string
--- components/ILIAS/ApiGateway/tests/Unit/Routing/RoutesRegistryTest.php
+++ components/ILIAS/ApiGateway/tests/Unit/Routing/RoutesRegistryTest.php
@@ -9,168 +9,81 @@ use ILIAS\ApiGateway\Routing\RoutesRegistry;
 use PHPUnit\Framework\TestCase;
 use LogicException;
 use InvalidArgumentException;
-use ReflectionProperty;
-use Error;
 
 final class RoutesRegistryTest extends TestCase
 {
     /** @var RoutesRegistry */
     private $registry;
 
+    #[\Override]
     protected function setUp(): void
     {
         parent::setUp();
-        // The RoutesRegistry is a singleton. To ensure a clean state for each test,
-        // we need to reset the singleton instance and its properties using reflection.
-        $this->resetSingleton(RoutesRegistry::getInstance());
-        $this->registry = RoutesRegistry::getInstance();
-    }
-
-    private function resetSingleton(RoutesRegistry $registry): void
-    {
-        $instance_property = new ReflectionProperty(RoutesRegistry::class, 'instance');
-        $instance_property->setAccessible(true);
-        $instance_property->setValue(null, null);
-
-        $routes_property = new ReflectionProperty($registry, 'routes');
-        $routes_property->setAccessible(true);
-        $routes_property->setValue($registry, []);
-    }
-
-    public function testItisSingleton(): void
-    {
-        $first_instance = RoutesRegistry::getInstance();
-        $second_instance = RoutesRegistry::getInstance();
-
-        self::assertSame($first_instance, $second_instance);
-    }
-
-    public function testThrowsExceptionOnUnserialize(): void
-    {
-        self::expectException(LogicException::class);
-        self::expectExceptionMessage('Cannot unserialize a singleton.');
-
-        $serialized = serialize(RoutesRegistry::getInstance());
-        unserialize($serialized);
-    }
-
-    public function testThrowsExceptionOnCloing(): void
-    {
-        $registry = RoutesRegistry::getInstance();
-
-        self::expectException(Error::class);
-
-        clone $registry;
     }
 
     public function testRegistersAndRetrievesRoute(): void
     {
-        $path = '/test';
-        $method = 'GET';
-        $route = $this->createMock(Route::class);
-        $route->method('getPath')->willReturn($path);
-        $route->method('getMethod')->willReturn($method);
+        $route = $this->createConfiguredMock(Route::class, [
+            'getPath' => $path = '/test',
+            'getMethod' => $method = 'GET',
+        ]);
 
-        $this->registry->register($route);
+        $key = "$method $path";
 
-        self::assertTrue($this->registry->has($method, $path));
-        self::assertSame($route, $this->registry->get($method, $path));
+        $registry = new RoutesRegistry([$route]);
+        $routes = $registry->all();
+
+        self::assertArrayHasKey($key, $routes);
+        self::assertSame($route, $routes[$key]);
     }
-
-
 
     public function testHandlesHttpMethodsCaseInsensitively(): void
     {
-        $path = '/test';
-        $route = $this->createMock(Route::class);
-        $route->method('getPath')->willReturn($path);
-        $route->method('getMethod')->willReturn('Post');
+        $route = $this->createConfiguredMock(Route::class, [
+            'getPath' => $path = '/test',
+            'getMethod' => $method = 'Post',
+        ]);
 
-        $this->registry->register($route);
+        $key = "POST $path";
 
-        self::assertTrue($this->registry->has('POST', $path));
-        self::assertSame($route, $this->registry->get('pOsT', $path));
+        $registry = new RoutesRegistry([$route]);
+        $routes = $registry->all();
+
+        self::assertArrayHasKey($key, $routes);
+        self::assertSame($route, $routes[$key]);
     }
 
     public function testThrowsExceptionForRouteWithEmptyMethod(): void
     {
-        $path = '/no-methods';
-        $route = $this->createMock(Route::class);
-        $route->method('getPath')->willReturn($path);
-        $route->method('getMethod')->willReturn('');
+        $route = $this->createConfiguredMock(Route::class, [
+            'getPath' => $path = '/no-methods',
+            'getMethod' => '',
+        ]);
 
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage("Cannot register a route with no HTTP methods for path '{$path}'.");
 
-        $this->registry->register($route);
+        new RoutesRegistry([$route]);
     }
 
     public function testThrowsExceptionInCaseOfDuplicateRoute(): void
     {
-        $path = '/duplicate';
-        $method = 'GET';
-        $route = $this->createMock(Route::class);
-
-        $route->method('getPath')->willReturn($path);
-        $route->method('getMethod')->willReturn($method);
-
-        $this->registry->register($route);
+        $route1 = $this->createConfiguredMock(Route::class, [
+            'getPath' => $path = '/duplicate',
+            'getMethod' => $method = 'GET',
+        ]);
+        $route2 = $this->createConfiguredMock(Route::class, [
+            'getPath' => $path,
+            'getMethod' => $method,
+        ]);
+        $route3 = $this->createConfiguredMock(Route::class, [
+            'getPath' => $path,
+            'getMethod' => 'POST',
+        ]);
 
         $this->expectException(LogicException::class);
         $this->expectExceptionMessage("Duplicate route detected: Cannot re-register 'GET {$path}'.");
 
-        $this->registry->register($route);
-    }
-
-    public function testReturnsNullForNonExistentpath(): void
-    {
-        self::assertNull($this->registry->get('GET', '/non-existent'));
-    }
-
-    public function testReturnsNullForNonExistentMethod(): void
-    {
-        $path = '/exists';
-        $route = $this->createMock(Route::class);
-        $route->method('getPath')->willReturn($path);
-        $route->method('getMethod')->willReturn('GET');
-        $this->registry->register($route);
-
-        self::assertNull($this->registry->get('POST', $path));
-    }
-
-    public function testReturnsFalseForNonExistentRoute(): void
-    {
-        self::assertFalse($this->registry->has('GET', '/non-existent'));
-    }
-
-    public function testReturnsEmptyArrayInitially(): void
-    {
-        self::assertEmpty($this->registry->all());
-    }
-
-    public function testReturnsAllRegisteredRoutes(): void
-    {
-        $this->markTestSkipped('Multiple methods are not currently supported.');
-
-        $route1 = $this->createMock(Route::class);
-
-        $route1->method('getPath')->willReturn('/route1');
-        $route1->method('getMethods')->willReturn(['GET']);
-
-        $route2 = $this->createMock(Route::class);
-
-        $route2->method('getPath')->willReturn('/route2');
-        $route2->method('getMethods')->willReturn(['POST', 'PUT']);
-
-        $this->registry->register($route1);
-        $this->registry->register($route2);
-
-        $expected = [
-            'GET /route1' => $route1,
-            'POST /route2' => $route2,
-            'PUT /route2' => $route2,
-        ];
-
-        self::assertEquals($expected, $this->registry->all());
+        new RoutesRegistry([$route1, $route2, $route3]);
     }
 }
