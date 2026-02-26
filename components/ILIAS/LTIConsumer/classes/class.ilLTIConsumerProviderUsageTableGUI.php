31eaf0e916d2e4809182c236423bdc7a13fabfdd
Saúl Díaz <sauldiazdiaz210@gmail.com>
2026-02-26 11:15:31 +0100

    php-cs-fixer fix

--- components/ILIAS/LTIConsumer/classes/class.ilLTIConsumerProviderUsageTableGUI.php
+++ components/ILIAS/LTIConsumer/classes/class.ilLTIConsumerProviderUsageTableGUI.php
@@ -106,7 +106,7 @@ class ilLTIConsumerProviderUsageTableGUI implements DataRetrieval
             fn($ret, $key, $value) => [$key, $value]
         );
 
-        usort($records, static function(array $left, array $right) use ($order_field): int {
+        usort($records, static function (array $left, array $right) use ($order_field): int {
             $left_val = $left[$order_field] ?? '';
             $right_val = $right[$order_field] ?? '';
             return ilStr::strCmp($left_val, $right_val);
