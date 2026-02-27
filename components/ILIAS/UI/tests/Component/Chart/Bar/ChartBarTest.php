f8ed08adb352d08ef1f3121057e736d0c926638e
Fabian Helfer <82493694+fhelfer@users.noreply.github.com>
2026-02-27 15:33:01 +0100

    [FIX] #47062 UI: remove unit from `Chart\Bar` height attribute. (#11016)
    * Fixes https://mantis.ilias.de/view.php?id=47062

--- components/ILIAS/UI/tests/Component/Chart/Bar/ChartBarTest.php
+++ components/ILIAS/UI/tests/Component/Chart/Bar/ChartBarTest.php
@@ -342,7 +342,7 @@ class ChartBarTest extends ILIAS_UI_TestBase
 
         $expected_html = <<<EOT
 <div class="il-chart-bar-horizontal">
-    <canvas id="id_1" height="150px" aria-label="bar123" role="img"></canvas>
+    <canvas id="id_1" height="150" aria-label="bar123" role="img"></canvas>
 </div>
 <div class="sr-only">
     <dl>
@@ -375,7 +375,7 @@ EOT;
 
         $expected_html = <<<EOT
 <div class="il-chart-bar-vertical">
-    <canvas id="id_1" height="165px" aria-label="bar123" role="img"></canvas>
+    <canvas id="id_1" height="165" aria-label="bar123" role="img"></canvas>
 </div>
 <div class="sr-only">
     <dl>
